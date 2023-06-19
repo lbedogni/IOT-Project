a0f1a1733c7dc1a96e21dfeb082be2a4c5a62ba3
Mattia <mattiasavoia97@gmail.com>
2023-06-19 14:39:54 +0200

    initial commit

---
+++ AirQuality/AQ_WEMOS/src/main.cpp
@@ -0,0 +1,161 @@
+#include <Arduino.h>
+
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+#include "AirQuality.h"
+
+
+#define NUMBER_OF_INPUTS 5
+#define NUMBER_OF_OUTPUTS 1
+
+bool isTest = true;
+int nIterTest = 100;
+
+
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+
+constexpr int kTensorArenaSize = 6*1024;
+uint8_t tensor_arena[kTensorArenaSize];
+
+
+void setup() {
+
+    Serial.begin(9600);
+
+    model = tflite::GetModel(AirQualityModel);
+    if (model->version() != TFLITE_SCHEMA_VERSION) {
+        Serial.print("Model provided is schema version not equal to supported!");
+        return;
+    } else {
+        Serial.print("Model loaded!\n");
+    }
+
+    // Questo richiama tutte le implementazioni delle operazioni di cui abbiamo bisogno
+    static tflite::AllOpsResolver resolver;
+
+    // Creo un interprete con cui eseguire il modello
+    static tflite::MicroInterpreter static_interpreter(model, resolver, tensor_arena, kTensorArenaSize);
+    interpreter = &static_interpreter;
+
+    // Alloco la memoria del tensor_arena per i tensori del modello
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+    if (allocate_status != kTfLiteOk) {
+        Serial.print("AllocateTensors() failed");
+        return;
+    } else {
+        Serial.print("AllocateTensors() done\n");
+    }
+
+    // Ottengo puntatori ai tensori di input e output del modello
+    input = interpreter->input(0);
+    output = interpreter->output(0);
+
+}
+
+void loop() {
+
+    if( isTest ){
+
+        if( nIterTest > 0 ){
+
+            float x_rand_test[ NUMBER_OF_INPUTS ];
+
+            // Inizializzo array con valore random che stano tra 0 e 1
+            for (int i = 0; i < NUMBER_OF_INPUTS; i++) {
+                x_rand_test[i] = static_cast<float>(rand()) / RAND_MAX;
+            }
+
+            // Array di output per la previsione del modello
+            float y_rand_pred[ NUMBER_OF_OUTPUTS ] = {0.0};
+
+            // Calcolo il tempo che impiega per la previsione del modello
+            uint32_t start = micros();
+
+            // Posiziono l'input nel tensore di input del modello
+            for(int x=0; x<NUMBER_OF_INPUTS; x++)
+                input->data.f[x] = x_rand_test[x];
+
+            // Eseguo l'inferenza e segnalo eventuali errori
+            TfLiteStatus invoke_status = interpreter->Invoke();
+            if (invoke_status != kTfLiteOk) {
+                Serial.print("Invoke failed!");
+                return;
+            }
+
+            // Ottengo l'output dal tensore di output del modello
+            if (output != NULL) {
+                for (int i = 0; i < NUMBER_OF_OUTPUTS; i++)
+                    y_rand_pred[i] = output->data.f[i];
+            }
+
+            uint32_t timePred = micros() - start;
+
+            Serial.print(timePred);
+
+            nIterTest -= 1;
+
+            Serial.print(nIterTest == 0 ? "\n" : ", ");
+
+            // Reset dell'interpreter
+            interpreter->Reset();  
+
+        }
+
+    } else {
+
+        // Dal file Header (AirQuality.h):
+        // un sample selezionato dal dataset -> x_test
+        // valore reale del sample selezionato dal dataset -> y_test
+
+        // Array di output per la previsione del modello
+        float y_pred[ NUMBER_OF_OUTPUTS ] = {0.0};
+
+        // Calcolo il tempo che impiega per la previsione del modello
+        uint32_t start = micros();
+
+        // Posiziono l'input nel tensore di input del modello
+        for(int x=0; x<NUMBER_OF_INPUTS; x++)
+            input->data.f[x] = x_test[x];
+
+        // Eseguo l'inferenza e segnalo eventuali errori
+        TfLiteStatus invoke_status = interpreter->Invoke();
+        if (invoke_status != kTfLiteOk) {
+            Serial.print("Invoke failed!");
+            return;
+        }
+
+        // Ottengo l'output dal tensore di output del modello
+        if (output != NULL) {
+            for (int i = 0; i < NUMBER_OF_OUTPUTS; i++)
+                y_pred[i] = output->data.f[i];
+        }
+
+        uint32_t timePred = micros() - start;
+
+        Serial.print("\n\nIt took ");
+        Serial.print(timePred);
+        Serial.println(" micros to run inference");
+
+        // Stampo il valore reale del sample preso in considerazione
+        Serial.print("Test output is: ");
+        Serial.println(y_test, 5);
+
+        // Stampo il risultato del modello
+        Serial.print("Predicted value is: ");
+        Serial.print(y_pred[0], 5);
+
+        // Reset dell'interpreter
+        interpreter->Reset();  
+
+        delay(60000);
+
+    }
+
+}
---
+++ HandwrittenDigits/HD_ESP32/src/main.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+#include <EloquentTinyML.h>
+#include "HandwrittenDigits.h"
+
+#define NUMBER_OF_INPUTS 8*8
+#define NUMBER_OF_OUTPUTS 10
+#define TENSOR_ARENA_SIZE 4*1024
+
+Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> model;
+
+
+bool isTest = false;
+int nIterTest = 100;
+
+
+void setup() {
+
+    Serial.begin(9600);
+
+    // Dal file Header (HandwrittenDigits.h):
+    // modello tensorflow -> HandwrittenDigitsModel
+
+    if ( ! model.begin(HandwrittenDigitsModel) ) {
+        Serial.println("Cannot load model!\n");
+        delay(60000);
+    } else {
+        Serial.print("Model loaded!\n");
+    }
+
+}
+
+void loop() {
+
+    if( isTest ){
+
+        if( nIterTest > 0 ){
+
+            float x_rand_test[ NUMBER_OF_INPUTS ];
+
+            // Array di output per la previsione del modello
+            float y_rand_pred[ NUMBER_OF_OUTPUTS ] = {0};
+
+            // Inizializzo array con valore random che stano tra 0 e 1
+            for (int i = 0; i < NUMBER_OF_INPUTS; i++) {
+                x_rand_test[i] = static_cast<float>(rand()) / RAND_MAX;
+            }
+
+            // Calcolo quanto inpiega la previsione
+            uint32_t start = micros();
+
+            model.predict(x_rand_test, y_rand_pred);
+
+            uint32_t timePred = micros() - start;
+
+            Serial.print(timePred);
+
+            nIterTest -= 1;
+
+            Serial.print(nIterTest == 0 ? "\n" : ", ");
+
+        }
+
+    } else {
+
+        // Dal file Header (HandwrittenDigits.h):
+        // un sample selezionato dal dataset -> x_test
+        // valore reale del sample selezionato dal dataset -> y_test
+
+        // Array di output per la previsione del modello
+        float y_pred[ NUMBER_OF_OUTPUTS ] = {0};
+
+        // Calcolo quanto impiega la previsione
+        uint32_t start = micros();
+
+        model.predict(x_test, y_pred);
+
+        uint32_t timePred = micros() - start;
+
+        Serial.print("\nIt took ");
+        Serial.print(timePred);
+        Serial.println(" micros to run inference !");
+
+        // Stampo il valore reale del sample preso in considerazione
+        Serial.print("Test output is: ");
+        Serial.println(y_test);
+
+        // Stampo il risultato del modello
+        // Sono le probabilità in riferimento ad ogni classe
+        Serial.print("Predicted proba are: ");
+        for (int i = 0; i < NUMBER_OF_OUTPUTS; i++) {
+            Serial.print(y_pred[i]);
+            Serial.print(i == NUMBER_OF_OUTPUTS-1 ? "\n" : ", ");
+        }
+
+        delay(60000);
+
+    }
+
+}
